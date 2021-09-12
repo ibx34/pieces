9cae81e687c9e041f848e8c807599f10b8d467d8
ibx34 <guamjust@gmail.com>
2021-09-11 22:21:03 -0400

    use the code danii gave me!!

--- src/lib.rs
+++ src/lib.rs
@@ -5,124 +5,104 @@ use bitflags::bitflags;
 // }
 
 #[derive(Debug)]
-pub struct FancyArgs
-{
-    pub inner: Vec<String>,
+
+pub struct FancyArgs {
+	pub inner: Vec<String>,
 }
 
-impl FancyArgs
-{
-    pub fn grab() -> FancyArgs
-    {
-        let mut args = std::env::args();
-        args.next();
+impl FancyArgs {
+	pub fn grab() -> FancyArgs {
+		let mut args = std::env::args();
 
-        FancyArgs {
-            inner: args.into_iter().collect::<Vec<String>>(),
-        }
-    }
+		args.next();
 
-    pub fn load(args: std::env::Args) -> FancyArgs
-    {
-        FancyArgs {
-            inner: args.into_iter().collect::<Vec<String>>(),
-        }
-    }
+		FancyArgs { inner: args.into_iter().collect::<Vec<String>>() }
+	}
+
+	pub fn load(args: std::env::Args) -> FancyArgs {
+		FancyArgs { inner: args.into_iter().collect::<Vec<String>>() }
+	}
 }
 
 #[derive(Debug)]
-pub struct Parser
-{
-    pub(crate) raw_args: FancyArgs,
-    pub args: Vec<args::Arg>,
+
+pub struct Parser {
+	pub(crate) raw_args: FancyArgs,
+	pub args: Vec<args::Arg>,
 }
 
-impl Parser
-{
-    // TODO after the app is created, add an argument here and to the struct
-    // for getting commands and such.
-    pub fn build(raw_args: FancyArgs, args: Option<Vec<args::Arg>>) -> Parser
-    {
-        Parser {
-            raw_args,
-            args: match args {
-                Some(a) => a,
-                None => vec![],
-            },
-        }
-    }
+impl Parser {
+	// TODO after the app is created, add an argument here and to the struct
+	// for getting commands and such.
+	pub fn build(raw_args: FancyArgs, args: Option<Vec<args::Arg>>) -> Parser {
+		Parser { raw_args,
+		         args: match args {
+			         Some(a) => a,
+		           None => vec![],
+		         } }
+	}
 }
 
-pub mod args
-{
+pub mod args {
 
-    #[derive(Debug, Clone)]
-    pub struct Arg
-    {
-        pub name: String,
-        pub long: Option<String>,
-        pub short: Option<String>,
-        pub help: Option<String>,
-        pub required: bool,
-    }
+	#[derive(Debug, Clone)]
 
-    impl Arg
-    {
-        pub fn new(name: String) -> Arg
-        {
-            Arg {
-                name,
-                long: None,
-                short: None,
-                help: None,
-                required: false,
-            }
-        }
+	pub struct Arg {
+		pub name: String,
+		pub long: Option<String>,
+		pub short: Option<String>,
+		pub help: Option<String>,
+		pub required: bool,
+	}
 
-        pub fn long(mut self, long: String) -> Self
-        {
-            self.long = Some(long);
-            self
-        }
+	impl Arg {
+		pub fn new(name: String) -> Arg {
+			Arg { name,
+			      long: None,
+			      short: None,
+			      help: None,
+			      required: false }
+		}
 
-        pub fn short(mut self, short: String) -> Self
-        {
-            self.short = Some(short);
-            self
-        }
+		pub fn long(mut self, long: String) -> Self {
+			self.long = Some(long);
 
-        pub fn help(mut self, help: String) -> Self
-        {
-            self.help = Some(help);
-            self
-        }
+			self
+		}
 
-        pub fn required(mut self, required: bool) -> Self
-        {
-            self.required = required;
-            self
-        }
-    }
+		pub fn short(mut self, short: String) -> Self {
+			self.short = Some(short);
 
-    pub fn check_args<'a>(args: &'a Vec<Arg>) -> (bool, Option<&'a Arg>, Option<&'a Arg>)
-    {
-        let mut args = args.iter();
+			self
+		}
 
-        while let Some(arg) = args.next() {
-            match args.find(|a| {
-                a.name == arg.name
-                    || a.short.is_some()
-                        && arg.short.is_some()
-                        && a.short.as_ref().unwrap() == arg.short.as_ref().unwrap()
-                    || a.long.is_some()
-                        && arg.long.is_some()
-                        && a.long.as_ref().unwrap() == arg.long.as_ref().unwrap()
-            }) {
-                Some(ark) => return (true, Some(&arg), Some(ark)),
-                None => continue,
-            }
-        }
+		pub fn help(mut self, help: String) -> Self {
+			self.help = Some(help);
 
-        return (false, None, None);
-    }
+			self
+		}
+
+		pub fn required(mut self, required: bool) -> Self {
+			self.required = required;
+
+			self
+		}
+	}
+
+	pub fn check_args<'a>(args: &'a Vec<Arg>) -> (bool, Option<&'a Arg>, Option<&'a Arg>) {
+		let mut args = args.iter();
+
+		while let Some(arg) = args.next() {
+			match args.find(|a| {
+				          a.name == arg.name
+				          || a.short == arg.short && a.short.is_some()
+				          || a.long == arg.long && a.long.is_some()
+			          }) {
+				Some(ark) => return (true, Some(&arg), Some(ark)),
+				None => continue,
+			}
+		}
+
+		return (false, None, None);
+	}
 }
