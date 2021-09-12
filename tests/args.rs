42082855fff7094487a59ec679641e2818ca3ce1
ibx34 <guamjust@gmail.com>
2021-09-12 16:34:31 -0400

    update tests, and add partialeq to FancyArgs

--- tests/args.rs
+++ tests/args.rs
@@ -1,13 +1,55 @@
 #[test]
 fn basic_arg() {
-    use pieces::args::{Arg,ArgSettings};
+	use pieces::args::{Arg, ArgSettings};
 
-    let correct_out = Arg { 
-        name: String::from("Foo"),
-        help: Some(String::from("Bar")),
-        long: None,
-        short: None,
-        settings: ArgSettings::empty()
-    };
-    assert_eq!(Arg::new(String::from("Foo")).help(String::from("Bar")), correct_out);
+	let correct_out = Arg {
+		name: String::from("Foo"),
+		help: Some(String::from("Bar")),
+		long: None,
+		short: None,
+		settings: ArgSettings::empty(),
+	};
+	assert_eq!(
+		Arg::new(String::from("Foo")).help(String::from("Bar")),
+		correct_out
+	);
+}
+
+#[test]
+fn long_arg() {
+	use pieces::args::{Arg, ArgSettings};
+
+	let correct_out = Arg {
+		name: String::from("Foo"),
+		help: Some(String::from("Bar")),
+		long: Some(String::from("Snack")),
+		short: None,
+		settings: ArgSettings::empty(),
+	};
+	assert_eq!(
+		Arg::new(String::from("Foo"))
+			.help(String::from("Bar"))
+			.long(String::from("Snack")),
+		correct_out
+	);
+}
+
+#[test]
+fn short_arg() {
+	use pieces::args::{Arg, ArgSettings};
+
+	let correct_out = Arg {
+		name: String::from("Foo"),
+		help: Some(String::from("Bar")),
+		long: Some(String::from("Snack")),
+		short: Some(String::from("Bar")),
+		settings: ArgSettings::empty(),
+	};
+	assert_eq!(
+		Arg::new(String::from("Foo"))
+			.help(String::from("Bar"))
+			.long(String::from("Snack"))
+			.short(String::from("Bar")),
+		correct_out
+	);
 }
